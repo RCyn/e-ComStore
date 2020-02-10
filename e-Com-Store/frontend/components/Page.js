d2ba817f836a7d115cdde63c84d54898916abcba
Cynthia <cynthiaren2000@gmail.com>
2020-02-10 15:35:29 -0500

    added styles to header and nav

--- e-Com-Store/frontend/components/Page.js
+++ e-Com-Store/frontend/components/Page.js
@@ -1,16 +1,70 @@
 import React from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 import Header from './Header';
 import Meta from './Meta';
 
+// TDDO: add light/dark theme
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightGrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url(/static/radnikanext-medium-webfont.woff2) format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 100%;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-size: 1rem;
+    font-family: 'radnika_next';
+    line-height: 2;
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 20px;
+`;
+
 class Page extends React.Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
--- e-Com-Store/frontend/components/styles/NavStyles.js
+++ e-Com-Store/frontend/components/styles/NavStyles.js
@@ -5,27 +5,27 @@ const NavStyles = styled.ul`
   padding: 0;
   display: flex;
   justify-self: end;
-  font-size: 2rem;
+  font-size: 1.25rem;
   a,
   button {
-    padding: 1rem 3rem;
+    padding: 10px 30px;
     display: flex;
     align-items: center;
     position: relative;
     text-transform: uppercase;
     font-weight: 900;
-    font-size: 1em;
+    font-size: 0.75em;
     background: none;
     border: 0;
     cursor: pointer;
     @media (max-width: 700px) {
-      font-size: 10px;
+      font-size: 0.75em;
       padding: 0 10px;
     }
     &:before {
       content: '';
       width: 2px;
-      background: ${props => props.theme.lightgrey};
+      background: ${props => props.theme.lightGrey};
       height: 100%;
       left: 0;
       position: absolute;
@@ -43,7 +43,7 @@ const NavStyles = styled.ul`
       transition: width 0.4s;
       transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
       left: 50%;
-      margin-top: 2rem;
+      margin-top: 20px;
     }
     &:hover,
     &:focus {
@@ -51,13 +51,13 @@ const NavStyles = styled.ul`
       &:after {
         width: calc(100% - 60px);
       }
-    @media (max-width: 700px) {
+      @media (max-width: 700px) {
         width: calc(100% - 10px);
-    }
+      }
     }
   }
-  @media (max-width: 1300px) {
-    border-top: 1px solid ${props => props.theme.lightgrey};
+  @media (max-width: 1400px) {
+    border-top: 1px solid ${props => props.theme.lightGrey};
     width: 100%;
     justify-content: center;
     font-size: 1.5rem;
