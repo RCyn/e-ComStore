fddf4c1be1cfc3147cb6af05361242dd768d3b91
Cynthia <cynthiaren2000@gmail.com>
2020-03-14 18:15:28 -0400

    add initial item mutation and query

--- e-Com-Store/backend/src/resolvers/Mutation.js
+++ e-Com-Store/backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutations = {};
+const Mutations = {
+  // db createItem returns a promise
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if user is logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- e-Com-Store/backend/src/resolvers/Query.js
+++ e-Com-Store/backend/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  // use same db logic
+  items: forwardTo('db'),
+};
 
 module.exports = Query;
